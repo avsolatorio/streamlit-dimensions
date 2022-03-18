a6316e54302cd155ffd9e5cf2706ab3cb641d23e
avsolatorio <avsolatorio@gmail.com>
2022-03-18 02:29:04 -0400

    Update setup.py and add README.md file.

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
+from os.path import dirname
+from os.path import join
 import setuptools
 
+def readme() -> str:
+    """Utility function to read the README file.
+    Used for the long_description.  It's nice, because now 1) we have a top
+    level README file and 2) it's easier to type in the README file than to put
+    a raw string in below.
+    :return: content of README.md
+    """
+
+    return open(join(dirname(__file__), "README.md")).read()
+
+
 setuptools.setup(
-    name="streamlit-custom-component",
+    name="streamlit-dimensions",
     version="0.0.1",
-    author="",
-    author_email="",
-    description="",
-    long_description="",
-    long_description_content_type="text/plain",
-    url="",
+    author="Aivin V. Solatorio",
+    author_email="avsolatorio@gmail.com",
+    description="A simple component that returns the dimensions of the container where it is rendered.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/avsolatorio/streamlit-dimensions",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
